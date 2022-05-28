2a012e1bc501598a801337f5728cf5e663e992e8
matheustakasaki <matheus8antunes@gmail.com>
2022-05-28 10:19:20 -0300

    Utilizando o if

---
+++ aula4/condicionais.js
@@ -0,0 +1,9 @@
+var nome = "Taa"
+
+if (nome === "Taka") {
+    console.log('Legal! Seu nome é este mesmo!')
+} else if (nome === 'Matheus') {
+    console.log('Ok, esse também serve');
+} else {
+    console.log('Que pena, seu nome não é Taka');
+}
