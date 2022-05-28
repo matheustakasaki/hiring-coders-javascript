1e3b0e8b86653c8ff1977717c14cd45cf319d56c
matheustakasaki <matheus8antunes@gmail.com>
2022-05-28 10:08:33 -0300

    Alguns exemplos de operações com variáveis

---
+++ aula2/operadores.js
@@ -0,0 +1,24 @@
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+console.log(resultado)
+
+// Concatenação
+var nome = 'matheus'
+
+var sobrenome = 'antunes'
+
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto)
+
+
+// NAN 
+var primeiroValor = 10;
+var segundoValor = 'azul';
+
+var resultadoValor = primeiroValor - segundoValor
+console.log(resultadoValor);
+
