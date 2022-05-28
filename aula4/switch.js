588c127ea2b6227067c738a8ce47c4224dc5f300
matheustakasaki <matheus8antunes@gmail.com>
2022-05-28 10:24:52 -0300

    Utilizando o switch

---
+++ aula4/switch.js
@@ -0,0 +1,14 @@
+
+var nome = 'lucas'
+
+switch (nome) {
+    case 'lucas':
+        console.log('Legal, vc é Lucas!');
+        break;
+    case 'Takasaki':
+        console.log('Também serve');
+        break
+    default:
+        console.log('Vc não é quem eu estou procurando');
+        break;
+}
